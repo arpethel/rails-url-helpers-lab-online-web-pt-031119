aaef9cf69d8f5c9cabfb2ee38dec306cdc281ba8
Ryan Pethel <arpethel@gmail.com>
2019-07-26 15:28:11 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,9 +11,10 @@ class StudentsController < ApplicationController
     # @active = Post.find(params[:id])
   end
 
-  private
-
-    def set_student
-      @student = Student.find(params[:id])
-    end
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
+  end
 end
